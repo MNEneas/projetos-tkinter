29465b44bf3565dbc82ff0a4499f4af787cfa684
MNEneas <eneasdeazevedojunior@gmail.com>
2026-06-05 00:07:38 -0400

    modificado para juntar o button no self

--- prim.py
+++ prim.py
@@ -8,12 +8,20 @@ class Application:
       self.msg = tk.Label(self.widget1, text="Primeiro widget")
       self.msg.pack ()
 
-      self.botao = tk.Button(self.widget1, text="Sair")
+      self.botao = tk.Button( #juntando tudo em um só
+          self.widget1,
+          text="Sair",
+          font=("Calibri", "10"),
+          width=5,
+          command=self.widget1.quit
+          )
+      
       #self.botao["text"] = ("Sair") daria para fazer isso também
-      self.botao["font"] = ("Calibri", "10")
-      self.botao["width"] = 5
-      self.botao["command"] = self.widget1.quit
-      self.botao.pack(side="right") #da para movimentar o botao com side e para onde ir
+      #self.botao["font"] = ("Calibri", "10")
+      #self.botao["width"] = 5
+      #self.botao["command"] = self.widget1.quit
+
+      self.botao.pack(side="left") #da para movimentar o botao com side e para onde ir
 
 
 root = tk.Tk()
