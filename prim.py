78f65398d1080f1c53d44a62b51151ac7ad65b9c
MNEneas <eneasdeazevedojunior@gmail.com>
2026-06-04 23:41:50 -0400

    Adicionado o botão sair

--- prim.py
+++ prim.py
@@ -8,6 +8,13 @@ class Application:
       self.msg = tk.Label(self.widget1, text="Primeiro widget")
       self.msg.pack ()
 
+      self.botao = tk.Button(self.widget1, text="Sair")
+      #self.botao["text"] = ("Sair") daria para fazer isso também
+      self.botao["font"] = ("Calibri", "10")
+      self.botao["width"] = 5
+      self.botao["command"] = self.widget1.quit
+      self.botao.pack(side="right") #da para movimentar o botao com side e para onde ir
+
 
 root = tk.Tk()
 Application(root)
